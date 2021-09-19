b6f06a5c9b113953a79b86b803b8f74c141591a5
Anastojkovicc <ana.stojkovic98@gmail.com>
2021-09-19 19:09:09 +0200

    Uspostavljena konekcija sa bazom i logovanje bibliotekara

---
+++ ProjekatZajednicki/src/rs/ac/bg/fon/ps/repository/db/impl/RepositoryDBUser.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.ps.repository.db.impl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import rs.ac.bg.fon.ps.domain.Bibliotekar;
+import rs.ac.bg.fon.ps.repository.db.DBRepository;
+
+/**
+ *
+ * @author ANA
+ */
+public class RepositoryDBUser implements DBRepository<Bibliotekar>{
+
+    @Override
+    public List<Bibliotekar> getAll() {
+        try {
+            String sql = "SELECT * FROM bibliotekar";
+            String url="jdbc:mysql://localhost:3306/biblioteka";
+            String username="root";
+            String password="";
+            
+            List<Bibliotekar> bibliotekari = new ArrayList<>();
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement s =  connection.createStatement();
+            ResultSet rs= s.executeQuery(sql);
+            while(rs.next()){
+                Bibliotekar b = new Bibliotekar();
+                b.setBibliotekarID(rs.getInt("bibliotekarID"));
+                b.setIme(rs.getString("ime"));
+                b.setPrezime(rs.getString("prezime"));
+                b.setUsername(rs.getString("username"));
+                b.setPassword(rs.getString("password"));
+                bibliotekari.add(b);
+            }
+            rs.close();
+            s.close();
+            return bibliotekari;
+        } catch (SQLException ex) {
+            Logger.getLogger(RepositoryDBUser.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+      
+    }
+    
+}
